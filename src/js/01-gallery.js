c29f790a8057f12a1ad7e06a325737015e27215b
Люда <1982luda30@gmail.com>
2022-07-22 15:37:42 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import '../css/common.css';
+import '../css/01-gallery.css';
+//console.log(galleryItems);
+const galleryDiv = document.querySelector('.gallery');
+//шаблон-sample
+const gallerySample = galleryItems.map((item) => {
+    return `<a class="gallery__item" 
+    href="${item.original}">
+  <img class="gallery__image" 
+  src="${item.preview}" 
+  alt="${item.description}" />
+</a>`
+})
+    .join('');
 
-console.log(galleryItems);
+galleryDiv.innerHTML = gallerySample;
+const lightbox = new SimpleLightbox('.gallery a',
+    {
+        captionsData: 'alt',
+        captionDelay: 250,
+    });
