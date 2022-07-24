01acd124d2312f1342f32bc0cd8e8c377f28033e
Люда <1982luda30@gmail.com>
2022-07-24 10:48:55 +0300

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,17 @@
-//import '../css/common.css';
-//import Player from '@vimeo/player';
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(iframe);
+
+const localStorageKey = 'videoplayer-current-time';
+
+function onTimeUpDate(event) {
+  localStorage.setItem(localStorageKey, event.seconds);
+}
+const saveTime = localStorage.getItem(localStorageKey);
+console.log(saveTime);
+if (saveTime) {
+  player.setCurrentTime(saveTime);
+}
+player.on('timeupdate', throttle(onTimeUpDate, 1000));
