34bdc867a60f537e652c70b9c0b9dc0be20b041c
Люда <1982luda30@gmail.com>
2022-07-25 00:25:48 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const localStorageKey = 'feedback-form-state';
+
+initialForm();
+
+form.addEventListener('input', throttle(onInput, 500));
+
+function onInput() {
+    const formData = new FormData(form);
+    const userForm = {};
+    formData.forEach((value, name) => (userForm[name] = value));
+    localStorage.setItem = (localStorageKey, JSON.stringify(userForm));
+};
+//сохраняемая форма
+function initialForm() {
+    const persistedForm = localStorage.getItem('localStorageKey');
+    if (persistedForm) {
+        persistedForm = JSON.parse(persistedForm);
+        console.log(persistedForm);
+        Object.entries(persistedForm).forEach(([name, value]) => {
+            form.elements[name].value = value;
+        })
+    }
+}
+//кнопка при отправке
+form.addEventListener('submit', onSubmit);
+function onSubmit(event) {
+    event.preventDefault();
+    localStorage.removeItem(localStorageKey);
+    const userForm = {};
+    const formData = new FormData(form);
+    formData.forEach((value, name) => (userForm[name] = value));
+    console.log(userForm);
+    form.reset();
+}
