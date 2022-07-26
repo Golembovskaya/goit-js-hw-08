b9a94120524406c0cc7792ddc799408661445e54
Люда <1982luda30@gmail.com>
2022-07-26 21:09:20 +0300

    localstor

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,6 +2,7 @@ import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
 const localStorageKey = 'feedback-form-state';
+//const selectedFilters = {};
 
 initialForm();
 
@@ -21,26 +22,40 @@ function onInput() {
     //const userForm = {};
     //formData.forEach((value, name) => (userForm[name] = value));
     //localStorage.setItem(localStorageKey, JSON.stringify(userForm));
-    };
-//сохраняемая форма
-function initialForm() {
-    const persistedForm = localStorage.getItem('localStorageKey');
-    if (persistedForm) {
-        persistedForm = JSON.parse(persistedForm);
-        console.log(persistedForm);
-        Object.entries(persistedForm).forEach(([name, value]) => {
-            form.elements[name].value = value;
-        })
-    }
 };
-//кнопка при отправке
+
+function initialForm() {
+    const persistedForm = localStorage.getItem(localStorageKey);
+    const persistedFormValue = JSON.parse(persistedForm);
+    if (persistedForm) {
+        form.elements.email.value = persistedFormValue.email;
+        form.elements.message.value = persistedFormValue.message;
+        //persistedForm = JSON.parse(persistedForm);
+        //console.log(persistedForm);
+        //Object.entries(persistedForm).forEach(([name, value]) => {
+            //selectedFilters[name] = value;
+            //form.elements[name].value = value;
+        }
+    }
 form.addEventListener('submit', onSubmit);
 function onSubmit(event) {
     event.preventDefault();
-    localStorage.removeItem(localStorageKey);
-    const userForm = {};
-    const formData = new FormData(form);
-    formData.forEach((value, name) => (userForm[name] = value));
-    console.log(userForm);
-    form.reset();
+    const formElements = event.currentTarget.elements;
+
+    const email = formElements.email.value;
+    const message = formElements.message.value;
+
+    const formDataElements = {
+    email,
+    message,
+  };
+  console.log(formDataElements);
+
+  form.reset();
+    //localStorage.removeItem(localStorageKey);
+    //const userForm = {};
+    //const formData = new FormData(form);
+    //formData.forEach((value, name) => console.log(value, name));
+    //console.log(userForm);
+    //form.reset();
 };
